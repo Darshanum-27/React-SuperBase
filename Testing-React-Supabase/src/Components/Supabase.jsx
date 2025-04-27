670400bb872e89e2d3edeaddd556c886d3da99ff
Darshan U M <109042286+Darshanum-27@users.noreply.github.com>
2025-04-27 18:04:46 -0500

    Update Supabase.jsx

--- Testing-React-Supabase/src/Components/Supabase.jsx
+++ Testing-React-Supabase/src/Components/Supabase.jsx
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const Supabase = createClient(
-    "https://uhuywinqzsnfgcmrpovu.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVodXl3aW5xenNuZmdjbXJwb3Z1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1OTg2MzgsImV4cCI6MjA2MTE3NDYzOH0._lAdZv6ES4bc0hMjaTfIXW0LMJnDjRUqZujl9DIu4j4"
+    "",
+    ""
   );
 
 export default Supabase;
